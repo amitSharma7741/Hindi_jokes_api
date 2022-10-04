ccc1b445437498ff319d7e96855c066375b8d3c7
amitSharma7741 <183099@nith.ac.in>
2022-10-04 23:28:16 +0530

    remove cors options

--- index.js
+++ index.js
@@ -3,37 +3,29 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
+dotenv.config({ path: './config.env' }); // to use the environment variables
 
  
-const corsOptions = {
-    origin: 'https://hindi-jokes-api.onrender.com/',
-    optionsSuccessStatus: 200,
-    credentials: true
 
-}
 
- app.use(cors(corsOptions));
+app.use(cors()); // to allow cross origin resource sharing
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // to allow cross origin resource sharing
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // to allow cross origin resource sharing
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // to allow cross origin resource sharing
     next();
 })
 
 
-const PORT = process.env.PORT || 5000;
-const Data = require("./Model/dataSchema");
-require("./DB/conn"); 
-// const connectDB = require("./DB/conn");
- 
+const PORT = process.env.PORT || 5000; // process.env.PORT is for render
+const Data = require("./Model/dataSchema"); // import dataSchema
+require("./DB/conn"); // connection to database
 
-app.use(express.json());
-app.use(require("./router/dataPost"));
-   
+app.use(express.json()); // to parse the data in json format
+app.use(require("./router/dataPost")); // import dataPost.js
+ 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
-    }
+}
 );
- 
